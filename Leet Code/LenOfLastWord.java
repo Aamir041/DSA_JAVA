1e3e2de630de67e988ea2b7986e8a036429c3abd
Aamir041 <aamirsaudagar34@gmail.com>
2023-02-19 02:46:42 +0530

    4ms 6.37%

---
+++ Leet Code/LenOfLastWord.java
@@ -0,0 +1,11 @@
+public class LenOfLastWord {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("Hello World"));
+    }
+    static int lengthOfLastWord(String s) {
+        if(s == "") return 0;
+        String[] strArr = s.trim().split("[\\s]");
+        return strArr[strArr.length-1].length();
+    }
+    
+}
