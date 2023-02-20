84921942c440ac624d5c0e02b3867ffe15d1afad
Aamir041 <aamirsaudagar34@gmail.com>
2023-02-20 10:20:08 +0530

    2ms 100%

---
+++ Leet Code/MedianOfTwoSortedArrays.java
@@ -0,0 +1,57 @@
+// link : https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+import java.util.Arrays;
+
+public class MedianOfTwoSortedArrays{
+    public static void main(String[] args) {
+        int[] nums1 = {1,2};
+        int[] nums2 = {3,4};
+        double res = findMedianSortedArrays(nums1,nums2);
+        System.out.println(res);
+    }
+    static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int mergedLength = nums1.length+nums2.length;
+        int[] mergedSortArr = new int[mergedLength];
+
+        int mergeIndex=0;
+        int i=0;
+        int j=0;
+        
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i] < nums2[j]){
+                mergedSortArr[mergeIndex] = nums1[i];
+                i++;
+                mergeIndex++; 
+            }
+            else{
+                mergedSortArr[mergeIndex] = nums2[j];
+                j++;
+                mergeIndex++;
+            }
+        }
+
+        while(i<nums1.length){
+            mergedSortArr[mergeIndex] = nums1[i];
+            i++;
+            mergeIndex++;
+        }
+
+        while(j<nums2.length){
+            mergedSortArr[mergeIndex] = nums2[j];
+            j++;
+            mergeIndex++;
+        }
+
+        double median = 0;
+        if(mergedLength % 2 == 0){
+            int z = mergedLength/2;
+            // foe
+            median = (mergedSortArr[z] + mergedSortArr[z-1])/2.0;
+        }
+        else{
+            int z = (mergedLength - 1)/2;
+            median = mergedSortArr[z]; 
+        }
+        return median;
+    }
+}
