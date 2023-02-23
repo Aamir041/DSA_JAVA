4fa556f331ea6f246fe01156ae3e6dea10f1f015
Aamir041 <aamirsaudagar34@gmail.com>
2023-02-23 19:41:49 +0530

    Linked List Selft Implementation Completed

--- Linked List/LinkListTut.java
+++ Linked List/LinkListTut.java
@@ -51,7 +51,139 @@ class LinkedList{
 		}
 		// incrementing length by +1
 		length++;
-	} 
+	}
+
+	public void removeLast(){
+		
+		if(head == null){
+			System.out.println("List is Empty");
+			return;
+		}
+		
+		if(length == 1){
+			head = null;
+			tail = null;
+			length--;
+			return;
+		}
+
+		Node temp = head;
+		Node pre = head;
+		
+		while(temp.next != null){
+			pre = temp;
+			temp = temp.next;
+		}
+
+		tail = pre;
+		tail.next=null;
+		length--;
+		
+	}
+
+	public void removeFirst(){
+		if(length == 0){
+			System.out.println("List is Empty");
+			return;
+		}
+
+		if(head == tail){
+			head = null;
+			tail = null;
+			length--;
+			return;
+		}
+		Node temp = head;
+		head = head.next;
+		temp.next = null;
+		length--;
+	}
+
+	public Node get(int index) {
+		
+		if(index > length || index < 0){
+			return null;
+		}
+
+		Node temp = head;
+		for(int i=0; i<index; i++){
+			temp = temp.next;
+		}
+
+		return temp; 
+	}
+
+	public boolean set(int index, int value){
+		if(index > length || index <0){
+			return false;
+		}
+
+		Node temp = get(index);
+
+		temp.value = value;
+
+		return true;
+	}
+
+	public boolean insert(int index,int value){
+		if(index >length || index <0){
+			return false;
+		}
+		if(index == 0){
+			prepend(value);
+			return true;
+		}
+
+		if(index == length-1){
+			append(value);
+			return true;
+		}
+
+		Node before = get(index-1);
+		Node newNode =  new Node(value);
+		newNode.next = before.next;
+		before.next = newNode;
+		length++;
+		return true;
+	}
+
+	public boolean remove(int index){
+		if(index <0 || index > length){
+			return false;
+		}
+		if(index == 0){
+			removeFirst();
+			return true;
+		}
+
+		if(index == length-1){
+			removeLast();
+			return true;
+		}
+		
+		Node before  = get(index-1);
+		Node deleteNode = before.next;
+		before.next = deleteNode.next;
+		deleteNode.next = null;
+		length--;
+		return true;
+
+	}
+
+	public void reverseLinkList(){
+		Node temp = head;
+		head = tail;
+		tail = head;
+		Node after = temp.next;
+		Node before = null;
+		
+		for(int i=0; i<length; i++){
+			after = temp.next;
+			temp.next = before;
+			before = temp;
+			temp = after;
+		}
+	}
 
 	public void printList(){
 		Node temp = head;
@@ -81,14 +213,16 @@ public class LinkListTut{
 		
 		LinkedList myLL = new LinkedList();
 		
-		myLL.append(2);
-		myLL.prepend(1);
-		myLL.append(3);
+		myLL.append(85);
+		myLL.prepend(69);
+		myLL.append(101);
+		myLL.append(112);
+		myLL.append(196);
 
 		myLL.printList();
 
-		myLL.getHead();
-		myLL.getTail();
-		myLL.getLength();
+		myLL.reverseLinkList();
+		
+		myLL.printList();
 	}
 }
