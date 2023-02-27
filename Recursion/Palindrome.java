341ebbf0148ed4a0078e4f540fbe6c2cb6e52938
Aamir041 <aamirsaudagar34@gmail.com>
2023-02-27 17:24:10 +0530

    removed boolean

--- Recursion/Palindrome.java
+++ Recursion/Palindrome.java
@@ -4,18 +4,18 @@ public class Palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        System.out.println(CheckPalindrome(str,0,true));
+        System.out.println(CheckPalindrome(str,0));
     }
 
-    static boolean CheckPalindrome(String str,int i,boolean result){
+    static boolean CheckPalindrome(String str,int i){
         int n = str.length();
-        if(i>=n/2 || !result){
-            return result;
+        if(i>=n/2){
+            return true;
         }
         if(str.charAt(i) != str.charAt(n-i-1)){
-            result = false;
+            return false;
         }
-        return CheckPalindrome(str,i+1,result);
+        return CheckPalindrome(str,i+1);
     }
 
 }
